6578423691921dab1ce074be09c0c43369aec71c
Thịnh - Niê Nguyễn <thinhnienguyen@gmail.com>
2022-06-27 12:17:53 +0700

    feat: add route and some components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  // basic routes
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'contact-us', component: ContactUsComponent },
+    ],
+  },
+  // login route
+  { path: 'login', component: LoginComponent },
+  // redirect to home
+  { path: '**', redirectTo: '' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    HomeComponent,
+    ContactUsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
