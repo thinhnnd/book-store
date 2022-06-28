b6c31e55993cc493318aa7454f638a10eafc392d
Thịnh - Niê Nguyễn <thinhnienguyen@gmail.com>
2022-06-28 12:05:40 +0700

    feat: username and password validate

---
+++ client/src/app/auth/mock-auth.service.ts
@@ -0,0 +1,1 @@
+import { IAuthService } from './i-auth.service';
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,27 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  username: string;
-  password: string;
   loginForm: FormGroup;
+  submitted = false;
+  returnUrl: string;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.username = '';
-    this.password = '';
-    this.loginForm = this.formBuilder.group({
-      email: '',
-      password: '',
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.returnUrl = '';
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
   }
 
-  ngOnInit(): void {}
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  ngOnInit(): void {
+    // this.loginForm = this.formBuilder.group({
+    //   username: ['', Validators.required],
+    //   password: ['', Validators.required],
+    // });
+    // get return url from route parameters or default to '/'
+    // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
 
   onLogin() {
+    this.submitted = true;
+    console.log('submited');
     console.log(this.loginForm);
   }
 }
